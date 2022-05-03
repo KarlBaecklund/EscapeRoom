fbb478532df333e6a6d58bc2d349a06d2cfaa5b9
KarlBaecklund <46314156+KarlBaecklund@users.noreply.github.com>
2022-05-03 17:21:44 +0200

    Controller Input

--- Assets/Karl Scripts/State.cs
+++ Assets/Karl Scripts/State.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class State : MonoBehaviour
 {
+    public List<GameObject> TextList = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
