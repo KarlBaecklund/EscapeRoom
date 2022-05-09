739d93f293fd7da4d2fb7bd1552cb49f6634b990
karl <Karl.baecklund@gmail.com>
2022-05-09 23:45:38 +0200

    JSON Update

--- Assets/Karl Scripts/JSONTest.cs
+++ Assets/Karl Scripts/JSONTest.cs
@@ -1,17 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 [System.Serializable]
-public class JSONTest
+public class JSONTest: MonoBehaviour
 {
-    public string puzzle;
-    public float time;
-}
+    public TextAsset textJSON;
 
-public class Main
-{
-    JSONTest test = new JSONTest();
+    [System.Serializable]
+    public class Puzzle
+    {
+        public string name;
+        public float time;
+        public int index;
+    }
 
-   
+    [System.Serializable]
+    public class PuzzleList
+    {
+        public Puzzle[] puzzle;
+    }
+
+
+    public Puzzle myPuzzle = new Puzzle();
+    public PuzzleList myPuzzleList = new PuzzleList();
+
+    public void outPutJSON()
+    {
+        string strOutPut = JsonUtility.ToJson(myPuzzleList);
+
+        File.WriteAllText("Assets/Karl Scripts/Text/JSONText.txt", strOutPut);
+    }
+
+    private void Start()
+    {
+        outPutJSON();
+
+    }
 }
